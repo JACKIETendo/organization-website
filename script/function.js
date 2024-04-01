50d0a620764d96bb9c51c7b8289a6962834ada2f
Nakanjako Tendo Jackline <ntendo4343@gmail.com>
2024-04-01 16:43:13 +0600

    changes

--- script/function.js
+++ script/function.js
@@ -17,7 +17,7 @@ document.getElementById('small_menu').addEventListener("click", function(){
 
 closeBtn.addEventListener("click",()=>{
     close.classList.remove('active');
-})
+});
 
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzSMQ6ocf95-RMzcPgWpWLNBJ6z8a7m7RK1TueXHrV2z8GaTwLD4UxyMmRCSyKwpmeH/exec'
@@ -35,4 +35,20 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbzSMQ6ocf95-RMzcPgWpW
             form.reset()
         })
         .catch(error => console.error('error!', error.message))
-    })
+    });
+    
+    
+function initMap() {
+    const myLatLng = {lat: 0.603499, lng: 32.655239};
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: myLatLng,
+    });
+    //add a marker to the map
+    const marker = new google.maps .Marker({
+        position: myLatLng,
+        map: map,
+        title: "Our location"
+    });
+}
